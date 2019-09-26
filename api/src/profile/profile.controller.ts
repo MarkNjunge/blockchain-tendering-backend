13d28a20590989951c4d2fb4d8ffd192e3dcfee3
MarkNjunge <mark.kamau@outlook.com>
2019-09-26 09:10:19 +0300

    Return error when fetching profile for NetworkAdmin

--- api/src/profile/profile.controller.ts
+++ api/src/profile/profile.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Get, HttpStatus, Req, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Req,
+  UseGuards,
+  HttpException,
+  BadRequestException,
+} from "@nestjs/common";
 import { ApiResponse } from "@nestjs/swagger";
 import { ProfileService } from "./profile.service";
 import { IncomingMessage } from "http";
 import { FastifyRequest } from "fastify";
 import { AuthGuard } from "src/common/guards/auth.guard";
 import { ProfileDto } from "./dto/profile.dto";
+import { SessionEntity } from "../db/entities/session.entity";
 
 @Controller("profile")
 @UseGuards(AuthGuard)
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
-  @Get("/")
+  @Get("/s")
   @ApiResponse({
     status: HttpStatus.OK,
     description: "Returns the current user's information",
@@ -20,6 +29,10 @@ export class ProfileController {
   async getProfile(
     @Req() req: FastifyRequest<IncomingMessage>,
   ): Promise<ProfileDto> {
+    const session: SessionEntity = req.params.session;
+    if (session.participantType === "NetworkAdmin") {
+      throw new BadRequestException("NetworkAdmins do not have profiles");
+    }
     return this.profileService.getProfile(req.params.session);
   }
 }
