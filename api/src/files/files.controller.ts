a09e74310ba46e151b39dc26571fbb7589f5304f
MarkNjunge <mark.kamau@outlook.com>
2019-10-12 20:44:22 +0300

    Use query instead of path for file download
    NestJS cant accept URL params longer than 100 characters
    https://github.com/nestjs/nest/issues/2065

--- api/src/files/files.controller.ts
+++ api/src/files/files.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Param, Get, Res } from "@nestjs/common";
+import {
+  Controller,
+  Param,
+  Get,
+  Res,
+  HttpException,
+  HttpStatus,
+  Query,
+} from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
 import * as fs from "fs";
 import { FastifyReply } from "fastify";
@@ -6,12 +14,16 @@ import { ServerResponse } from "http";
 
 @Controller("files")
 export class FilesController {
-  @Get("/documents/:ref")
+  @Get("/documents")
   @ApiOperation({ title: "Download a document" })
   async download(
-    @Param("ref") ref: string,
+    @Query("ref") ref: string,
     @Res() res: FastifyReply<ServerResponse>,
   ) {
+    if (!fs.existsSync(`./files/documents/${ref}`)) {
+      throw new HttpException("File not found", HttpStatus.NOT_FOUND);
+    }
+
     const file = fs.createReadStream(`./files/documents/${ref}`);
 
     const fullFilename = new Buffer(ref, "base64").toString();
